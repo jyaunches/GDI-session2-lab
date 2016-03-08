f2ed94c7a27764d41536c1996ac811c3d1b44322
jyaunches <jmyaunch@gmail.com>
2016-03-08 11:30:21 -0500

    Initial Commit

---
+++ GDI-session2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GDI-session2
+//
+//  Created by julie.yaunches on 3/8/16.
+//  Copyright © 2016 julie.yaunches. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
